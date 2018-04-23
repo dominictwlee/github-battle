37eb6d6357f7955d462b8a3e50b9c0d341808c44
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-23 21:35:08 +0100

    add public path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './app/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   devServer: {
     historyApiFallback: true
