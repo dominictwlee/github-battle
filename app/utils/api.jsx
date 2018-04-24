7df231851292dd4b649868cd0b7fff13db335d51
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-24 17:54:46 +0100

    Create functions to fetch from github API

--- app/utils/api.jsx
+++ app/utils/api.jsx
@@ -1,6 +1,57 @@
 import axios from 'axios';
 
-export default function fetchPopularRepos(language) {
+const id = process.env.CLIENT_ID;
+const secret = process.env.CLIENT_SECRET;
+const params = `?client_id=${id}&client_secret=${secret}`;
+
+function getProfile(username) {
+  return axios.get(`https://api.github.com/users/${username}${params}`).then(user => user.data);
+}
+
+function getRepos(username) {
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`).then(repos => repos.data);
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, repo) => count + repo.stargazers_count, 0);
+}
+
+function calculateScore(profile, repos) {
+  const { followers } = profile;
+  const totalStars = getStarCount(repos);
+
+  return followers * 3 + totalStars;
+}
+
+function handleError(err) {
+  console.error(err);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(data => {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  return axios
+    .all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+}
+
+export function fetchPopularRepos(language) {
   const encodedURI = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './app/index.jsx',
@@ -35,7 +36,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new Dotenv()
   ],
   mode: 'development'
 };
